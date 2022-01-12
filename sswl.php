5e8717563530947aa70c5c4a8596b67b44813461
cuishilei <18310120387@163.com>
2022-01-12 08:04:26 +0800

    fix

---
+++ sswl.php
@@ -0,0 +1,51 @@
+
+4000121321 SSWL
+
+//phpstorm 破解
+https://pdd.ismicool.cn/idea/
+
+https://pdd.ismicool.cn/idea/code.html
+
+//Hyper-v
+bcdedit /set hypervisorlaunchtype off    //Hyper-v 关闭
+bcdedit /set hypervisorlaunchtype on    //Hyper-v 开启
+DISM /Online /Enable-Feature /All /FeatureName:Microsoft-Hyper-V
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
+
+
+tail -f /d/phpstudy_pro/WWW/tms_admin/protected/runtime/logs/error.log |grep 'csl'
+
+/d/phpstudy_pro/WWW/yii3/basic/yii csl/cui 
+
+
+/添加索引
+ALTER  TABLE  `table_name`  ADD  INDEX index_name (  `column`  )
+
+//添加字段
+ALTER table tms_supplier add COLUMN tms_sup_bank_account VARCHAR (255) DEFAULT '' COMMENT '银行账号' AFTER tms_sup_invoicetype;
+ALTER table tms_supplier add COLUMN tms_sup_bank_name VARCHAR (255) DEFAULT '' COMMENT '开户银行名称' AFTER tms_sup_invoicetype;
+ALTER table tms_supplier add COLUMN tms_sup_payee_name VARCHAR (255) DEFAULT '' COMMENT '银行账号' AFTER tms_sup_invoicetype;
+
+
+ALTER table car add COLUMN car_volume int (11) DEFAULT 0 COMMENT '车辆容积' AFTER car_lastuse_time;
+ALTER table car add COLUMN car_ratify_size VARCHAR (255) DEFAULT '' COMMENT '核定大小' AFTER car_lastuse_time;
+
+
+ALTER table tms_mf_shipnoadd add COLUMN tms_mf_send_month VARCHAR (15) DEFAULT '' COMMENT '发货月份' AFTER tms_mf_createday;
+ALTER table tms_mf_shipnoadd add COLUMN tms_mf_carriers_name VARCHAR (255) DEFAULT '' COMMENT '物流承运商' AFTER tms_mf_createday;
+
+//修改类型
+ alter table tms_mf_shipnoadd modify column tms_mf_send_month varchar(15) COMMENT '发货月份';
+
+密码 
+ cuishilei Cui123
+ 60988387
+
+git  http://2587984bx6.zicp.vip
+cuishilei   aiuz4mz_ggU5*'kO
+cuishilei   123456
+18310120387@163.com    k7el2*B{vxfpzp~R  yapi.ashsh.com.cn
+
+生生git
+http://2587984bx6.zicp.vip/dashboard/projects
+
