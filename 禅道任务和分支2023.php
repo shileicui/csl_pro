6a438b173f82790377a0e4d82068710c659dde35
cuishilei <18310120387@163.com>
2023-08-07 19:09:59 +0800

    dd

--- 禅道任务和分支2023.php
+++ 禅道任务和分支2023.php
@@ -23,3 +23,38 @@ CREATE TABLE `tms_car_break` (
   `deleted_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '删除时间',
   PRIMARY KEY (`tar_id`)
 ) COMMENT='车辆违章记录';
+
+
+
+CREATE TABLE `tms_order_druglog` (
+  `tod_id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `ur_uid` int(11) NOT NULL DEFAULT '0' COMMENT '操作员id',
+  `username` varchar(30) NOT NULL DEFAULT '' COMMENT '操作员名称',
+  `to_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '订单to_id',
+  `temp_number` varchar(50) NOT NULL DEFAULT '' COMMENT 'Libero温度计',
+  `out_temp_number` varchar(50) NOT NULL COMMENT '外显温度计',
+  `box_number` varchar(50) NOT NULL DEFAULT '' COMMENT '箱子序列号',
+  `sync_status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '是否同步主表  0未同步  1已同步',
+  `tod_visible` tinyint(4) NOT NULL DEFAULT '1' COMMENT '状态  1正常  2删除',
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updated_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '修改时间',
+  `deleted_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '删除时间',
+  PRIMARY KEY (`tod_id`)
+)COMMENT='操作员订单药品信息表';
+
+
+CREATE TABLE `tms_orderdrug_infolog` (
+  `toi_id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `ur_uid` int(11) NOT NULL DEFAULT '0' COMMENT '操作员id',
+  `username` varchar(30) NOT NULL DEFAULT '' COMMENT '操作员名称',
+  `tod_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT 'tms_order_druglog表 tod_id',
+  `drug_number` varchar(50) NOT NULL DEFAULT '' COMMENT '药品序列号',
+  `sync_status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '是否同步主表  0未同步  1已同步',
+  `toi_visible` tinyint(4) NOT NULL DEFAULT '1' COMMENT '状态  1正常  2删除',
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updated_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '修改时间',
+  `deleted_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '删除时间',
+  PRIMARY KEY (`toi_id`)
+)COMMENT='操作员订单药品信息关联表';
+
+ALTER table tms_order_drug add COLUMN log_tod_id  tinyint(4)  not null  DEFAULT '0' COMMENT '操作记录表tod_id';
