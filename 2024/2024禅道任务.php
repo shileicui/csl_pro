602cc9a875f60c620cf98873bf2465f488e762b1
cuishilei <18310120387@163.com>
2024-10-22 18:46:03 +0800

    供应商小程序

--- 2024/2024禅道任务.php
+++ 2024/2024禅道任务.php
@@ -2548,7 +2548,12 @@ https://project.ashsh.com.cn/index.php?m=task&f=view&taskID=21638  未上线
 
 
 主题
-tms.fahuo.add.receiptno
+// tms.fahuo.add.receiptno
+
+
+order.fourth.delivery 
+
+tms.order.fadb.update 
 
 异步接收地址
 index.php?r=tms-su-shipment/update-status
@@ -2556,6 +2561,7 @@ index.php?r=tms-su-shipment/update-status
 CREATE TABLE `tms_supplier_operator` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键id',
   `su_id` int(11) NOT NULL DEFAULT '0' COMMENT '所属供应商id',
+  `ur_uid` int(11) NOT NULL DEFAULT '0' COMMENT '用户表id(user.ur_uid)',
   `operator` varchar(20) NOT NULL DEFAULT '' COMMENT '操作人名称',
   `operator_phone` varchar(15) NOT NULL DEFAULT '0' COMMENT '操作人手机',
   `op_token` varchar(50) NOT NULL DEFAULT '' COMMENT '登录token',
@@ -2569,6 +2575,7 @@ CREATE TABLE `tms_supplier_operator` (
   PRIMARY KEY (`id`),
   KEY `idx_su_id` (`su_id`),
   KEY `idx_phone` (`operator_phone`),
+  KEY `idx_ur_uid` (`ur_uid`),
   KEY `idx_token` (`op_token`)
 ) COMMENT='供应商操作人表';
 
@@ -2594,8 +2601,8 @@ CREATE TABLE `tms_su_shipment` (
   `tss_shipment_no` varchar(255)  NOT NULL DEFAULT '' COMMENT '发货单号',
   `tss_type` tinyint(4)  NOT NULL DEFAULT '1'  COMMENT '货物类型 1、干冰 2、锂电池 3、其他',
   `tss_quantity` int(10) NOT NULL DEFAULT '0' COMMENT '总件数',
-  `standard_weight` int(10) NOT NULL DEFAULT '0' COMMENT '标准重量',
-  `actual_weight` int(10) NOT NULL DEFAULT '0' COMMENT '实际重量',
+  `standard_weight` decimal(10,2) NOT NULL DEFAULT '0.00' COMMENT '标准重量',
+  `actual_weight` decimal(10,2) NOT NULL DEFAULT '0.00' COMMENT '实际重量',
   `actual_remark` text COMMENT '发货备注',
   `register_state` tinyint(4) NOT NULL DEFAULT '1'  COMMENT '费用登记状态 1、未登记 2、已登记 3、已通过 4、已驳回',
   `tss_state` tinyint(4) NOT NULL DEFAULT '0'  COMMENT '发货明细变更状态 0未变更 1 已变更',
@@ -2612,10 +2619,14 @@ CREATE TABLE `tms_su_shipment` (
   `total_money`  decimal(10,2) NOT NULL DEFAULT '0.00'  COMMENT '总金额',
 
   `expense_remark` text COMMENT '费用备注',
+  `cause_rejection` text COMMENT '驳回原因',
 
+  `operator_phone` varchar(15) NOT NULL DEFAULT '' COMMENT '操作人手机',
+
+  `distask_state` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1：未提货，2：已提货',
   `tss_visible` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1：显示，2：删除',
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `updated_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '修改时间',
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
   `deleted_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '删除时间',
   PRIMARY KEY (`tss_id`),
   KEY `idx_ro_id` (`ro_id`) ,
@@ -2630,13 +2641,13 @@ CREATE TABLE `tms_su_shipment` (
 
 CREATE TABLE `tms_su_shipment_order` (
   `tsso_id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键ID',
-  `tss_id` int(11) NOT NULL DEFAULT '0' COMMENT '供应商发货ID （关联tms_su_shipment.tss_id',
+  `tss_id` int(11) NOT NULL DEFAULT '0' COMMENT '供应商发货ID （关联tms_su_shipment.tss_id)',
   `to_id`  int(11) unsigned  NOT NULL DEFAULT '0' COMMENT '订单id(关联transport_order.to_id)',
   `di_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '调度ID （关联dispatch.di_id）',
   `tsso_quantity` int(10) NOT NULL DEFAULT '0' COMMENT '发货数量',
   `tsso_visible` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1：显示，2：删除',
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `updated_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '修改时间',
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
   `deleted_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '删除时间',
   PRIMARY KEY (`tsso_id`),
   KEY `idx_tss_id` (`tss_id`) ,
@@ -2656,7 +2667,7 @@ CREATE TABLE `tms_su_shipment_box` (
   `temp_text` varchar(255) NOT NULL DEFAULT '' COMMENT '箱子对应的温度计',
   `tssb_visible` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1：显示，2：删除',
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `updated_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '修改时间',
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
   `deleted_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '删除时间',
   PRIMARY KEY (`tssb_id`),
   KEY `idx_sto_id` (`sto_id`) ,
@@ -2673,7 +2684,7 @@ CREATE TABLE `tms_su_shipment_route` (
   `tssr_state` tinyint(4) NOT NULL DEFAULT '1'  COMMENT '发货状态 1、部分发货 2、已发货',
   `tssr_visible` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1：显示，2：删除',
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `updated_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '修改时间',
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
   `deleted_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '删除时间',
   PRIMARY KEY (`tssr_id`),
   KEY `idx_ro_id` (`ro_id`) ,
@@ -2692,14 +2703,14 @@ CREATE TABLE `tms_supplier_operator_order` (
   `tsoo_state` tinyint(4) NOT NULL DEFAULT '1' COMMENT '是否确认发货 1 未确认发货 2 已确认发货 ',
   `tsoo_visible` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1：显示，2：删除',
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `updated_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '修改时间',
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
   `deleted_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '删除时间',
   PRIMARY KEY (`tsoo_id`),
   KEY `idx_tssr_id` (`tssr_id`) ,
   KEY `idx_tso_id` (`tso_id`) ,
   KEY `idx_di_id` (`di_id`) ,
   KEY `idx_to_id` (`to_id`) 
-) COMMENT='供应商操作人表扫码运单表';
+) COMMENT='供应商操作人表发货扫码运单表';
 
 
 ALTER table
@@ -2708,6 +2719,173 @@ add
   COLUMN `tss_su_id`   int(11) NOT NULL DEFAULT '0' COMMENT '代理供应商id（关联 tms_supplier.tms_sup_id)',
 add
   COLUMN `tss_su_name`   varchar(255) NOT NULL DEFAULT '' COMMENT '代理供应商';
+
+
+// ALTER TABLE tms_su_shipment MODIFY COLUMN `standard_weight` decimal(10,2) NOT NULL DEFAULT '0.00' COMMENT '标准重量',
+// MODIFY COLUMN `actual_weight` decimal(10,2) NOT NULL DEFAULT '0.00' COMMENT '实际重量';
+
+
+//提货--------------------lllllllllllllllllllllllll----------------------、、、、、、、
+
+
+
+
+
+
+
+// ALTER table
+//   tms_su_shipment
+// add
+//   COLUMN `tsd_id`   int(11) NOT NULL DEFAULT '0' COMMENT '关联提货id（关联 tms_supplier_distask.tsd_id)';
+
+
+CREATE TABLE `tms_supplier_operator_distask` (
+  `tsod_id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+
+  `tso_id` int(11) NOT NULL DEFAULT '0' COMMENT '供应商操作人表ID （关联tms_supplier_operator.id)',
+  `region_id` int(11) NOT NULL DEFAULT '0' COMMENT '城市ID （关联route.stop_region_id)',
+  `tss_ids` varchar(255) NOT NULL DEFAULT '' COMMENT '扫描选择的发货明细id tms_su_shipment.tss_id',
+
+  `distask_time` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '提货日期',
+  `to_id`  int(11) unsigned  NOT NULL DEFAULT '0' COMMENT '订单id(关联transport_order.to_id)',
+  `di_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '调度ID （关联dispatch.di_id）',
+
+  `tsod_state` tinyint(4) NOT NULL DEFAULT '1' COMMENT '是否确认提货 1 未确认提货 2 已确认提货 ',
+  `tsod_visible` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1：正常，2：删除',
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  `deleted_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '删除时间',
+  PRIMARY KEY (`tsod_id`),
+  KEY `idx_tso_id` (`tso_id`) ,
+  KEY `idx_distask_time` (`distask_time`) ,
+  KEY `idx_di_id` (`di_id`) ,
+  KEY `idx_to_id` (`to_id`) 
+) COMMENT='供应商操作人表提货扫码运单表';
+
+
+
+CREATE TABLE `tms_supplier_distask` (
+  `tsd_id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+
+  `distask_time` int(11) unsigned  NOT NULL DEFAULT '0' COMMENT '提货时间',
+
+  `region_name` varchar(128)  NOT NULL DEFAULT '' COMMENT '提货城市名称',
+  `region_id` int(11) NOT NULL DEFAULT '0' COMMENT '城市ID （关联route.stop_region_id)',
+  `su_id`   int(11) NOT NULL DEFAULT '0' COMMENT '供应商id（关联 tms_supplier.tms_sup_id)',
+
+  `unit_price`  decimal(10,2) NOT NULL DEFAULT '0.00'  COMMENT '单价',
+  `distask_price`  decimal(10,2) NOT NULL DEFAULT '0.00'  COMMENT '提货费',
+  `service_price`  decimal(10,2) NOT NULL DEFAULT '0.00'  COMMENT '服务费',
+  `other_price`  decimal(10,2) NOT NULL DEFAULT '0.00'  COMMENT '其他收费',
+
+  `tsd_quantity` int(10) NOT NULL DEFAULT '0' COMMENT '总件数',
+  `tsd_order_num` int(10) NOT NULL DEFAULT '0' COMMENT '订单单数',
+
+  `operator_phone` varchar(15) NOT NULL DEFAULT '' COMMENT '操作人手机',
+  `tsd_visible` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1：正常，2：删除',
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  `deleted_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '删除时间',
+  PRIMARY KEY (`tsd_id`),
+  KEY `idx_distask_time` (`distask_time`) 
+) COMMENT='供应商提货表明细';
+
+
+
+// CREATE TABLE `tms_supplier_distask_relevance` (
+//   `tdsr_id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+
+//   `tsd_id` int(11) NOT NULL DEFAULT '0' COMMENT '供应商提货ID tms_supplier_distask.tsd_id)',
+
+//   `tss_id` int(11) NOT NULL DEFAULT '0' COMMENT '供应商发货ID （关联tms_su_shipment.tss_id)',
+
+//   `tdsr_quantity` int(10) NOT NULL DEFAULT '0' COMMENT '提货数量',
+//   `tdsr_order_num` int(10) NOT NULL DEFAULT '0' COMMENT '订单数量',
+
+//   `tdsr_state` tinyint(4) NOT NULL DEFAULT '1' COMMENT '是否确认发货 1 显示 2 隐藏 ',
+//   `tdsr_visible` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1：正常，2：删除',
+//   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+//   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+//   `deleted_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '删除时间',
+//   PRIMARY KEY (`tdsr_id`),
+//   KEY `idx_tsd_id` (`tsd_id`) ,
+//   KEY `idx_tss_id` (`tss_id`) 
+// ) COMMENT='供应商提货关联表';
+
+
+CREATE TABLE `tms_su_shipment_order_distask` (
+  `tssod_id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+
+  `tsso_id` int(11) NOT NULL DEFAULT '0' COMMENT '发货明细订单表id(关联tms_su_shipment_order.tsso_id)',
+  `tsd_id` int(11) NOT NULL DEFAULT '0' COMMENT '供应商提货ID tms_supplier_distask.tsd_id)',
+
+  `tss_id` int(11) NOT NULL DEFAULT '0' COMMENT '供应商发货ID （关联tms_su_shipment.tss_id)',
+  `to_id`  int(11) unsigned  NOT NULL DEFAULT '0' COMMENT '订单id(关联transport_order.to_id)',
+
+  `di_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '调度ID （关联dispatch.di_id）',
+  `tssod_quantity` int(10) NOT NULL DEFAULT '0' COMMENT '提货数量',
+
+  `tssod_state` tinyint(4) NOT NULL DEFAULT '1' COMMENT '是否确认发货 1 显示 2 隐藏 ',
+  `tssod_visible` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1：正常，2：删除',
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updated_at`  datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  `deleted_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '删除时间',
+  PRIMARY KEY (`tssod_id`),
+  KEY `idx_tss_id` (`tss_id`) ,
+  KEY `idx_tsso_id` (`tsso_id`) ,
+  KEY `idx_di_id` (`di_id`) ,
+  KEY `idx_tsd_id` (`tsd_id`) ,
+  KEY `idx_to_id` (`to_id`) 
+) COMMENT='供应商提货订单表';
+
+
+CREATE TABLE `tms_su_shipment_box_distask` (
+  `tssbd_id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+
+  `to_id`  int(11) unsigned  NOT NULL DEFAULT '0' COMMENT '订单id(关联transport_order.to_id)',
+  `tssod_id`  int(11) unsigned  NOT NULL DEFAULT '0' COMMENT '提货明细订单表id(tms_su_shipment_order_distask.tssod_id)',
+  `tsso_id`  int(11) unsigned  NOT NULL DEFAULT '0' COMMENT '发货明细订单表id(关联tms_su_shipment_order.tsso_id)',
+
+  `sto_id`  int(11) unsigned  NOT NULL DEFAULT '0' COMMENT '箱型sto_id(关联order_material.sto_id)',
+  `sto_no` char(32)  NOT NULL DEFAULT '' COMMENT '存货编号',
+
+  `tssbd_quantity` int(10) NOT NULL DEFAULT '0' COMMENT '提货数量',
+  `box_weight` decimal(10,2) NOT NULL DEFAULT '0.00' COMMENT '单个箱子的标准重量',
+  `temp_text` varchar(255) NOT NULL DEFAULT '' COMMENT '箱子对应的温度计',
+
+  `tssbd_state` tinyint(4) NOT NULL DEFAULT '1' COMMENT '是否确认发货 1 显示 2 隐藏 ',
+  `tssbd_visible` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1：正常，2：删除',
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  `deleted_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '删除时间',
+  PRIMARY KEY (`tssbd_id`),
+  KEY `idx_sto_id` (`sto_id`) ,
+  KEY `idx_tssod_id` (`tssod_id`) ,
+  KEY `idx_tsso_id` (`tsso_id`) ,
+  KEY `idx_to_id` (`to_id`) 
+) COMMENT='供应商提货表对应箱型';
+
+
+ALTER table
+  tms_su_shipment
+add
+ `distask_state` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1：未提货，2：已提货';
+
+ALTER table
+  tms_supplier_operator_distask
+add
+ `tss_ids` varchar(255) NOT NULL DEFAULT '' COMMENT '扫描选择的发货明细id tms_su_shipment.tss_id';
+
+
+
+// alter table ams_external_assist_materials drop su_name;
+
+
+// ALTER table
+//   tms_supplier_operator_distask
+// ADD
+//   COLUMN `tss_ids` varchar(255)  NOT NULL DEFAULT '' COMMENT '扫码选择的发货明细ids tms_su_shipment.tss_id';
+
 
 // ALTER table
 //   tms_su_shipment_box
@@ -2750,13 +2928,34 @@ add
 // ALTER table
 //   tms_su_shipment
 // add
-//   COLUMN `tss_state` tinyint(4) NOT NULL DEFAULT '0'  COMMENT '发货明细变更状态 0未变更 1 已变更';
+//   COLUMN `tss_operator_phone` varchar(15) NOT NULL DEFAULT '0' COMMENT '操作人手机';
+
+
+// ALTER table
+//   tms_su_shipment
+// add
+//   COLUMN `cause_rejection` text COMMENT '驳回原因';
 
 
 
-ALTER TABLE tms_su_shipment_route
-ADD UNIQUE KEY `uniq_roid_time_index` (`ro_id`, `departure_trans_time`);
+// ALTER TABLE tms_su_shipment_route
+// ADD UNIQUE KEY `uniq_roid_time_index` (`ro_id`, `departure_trans_time`);
 
+
+// UPDATE tms_su_shipment tss
+// LEFT JOIN tms_remark_log trl ON tss.tss_id = trl.trl_identifier and trl_subtype =158001
+// LEFT JOIN tms_supplier_operator tso ON tso.id = trl.trl_uid  
+// SET tss.tss_operator_phone = tso.operator_phone;
+
+
+to_identify =2 多温区
+
+ALTER table
+  tms_supplier_operator
+ADD
+  COLUMN  `ur_uid` int(11) NOT NULL DEFAULT '0' COMMENT '用户表id(user.ur_uid)',
+ADD
+  INDEX idx_ur_uid (`ur_uid`);
 
 if (!RedisLock::lock("tdisupdate:lock",$to_id)) {
                 throw new \Exception('当前订单修改调度中，已锁定');
@@ -2765,7 +2964,7 @@ if (!RedisLock::lock("tdisupdate:lock",$to_id)) {
             RedisLock::unlock("tdisupdate:lock",$to_id);
 
 
-ALTER TABLE tms_su_shipment_detail RENAME TO tms_su_shipment_order;
+// ALTER TABLE tms_su_shipment_detail RENAME TO tms_su_shipment_order;
 
 
 车辆维保信息导出优化  已上线  8.19
@@ -2992,3 +3191,97 @@ INSERT INTO `remark_log` (`rl_type`, `rl_subtype`, `rl_identifier`, `ur_uid`, `u
 路桥费导出优化
 feature_22137_csl_20240918
 https://project.ashsh.com.cn/index.php?m=task&f=view&taskID=22137
+
+
+
+update tms_refuel_record set sm_name = '中国石化销售股份有限公司广东广州石油分公司',account_province='广东省',payment_company='生生供应链管理（广州）有限公司',main_card_number='1000114400027509580',trr_card_no='1000114400028206565' where trr_id='52233'
+
+INSERT INTO `tms_remark_log` (`trl_type`, `trl_subtype`, `trl_identifier`, `trl_uid`, `trl_username`, `trl_status`, `trl_remark`, `trl_createtime`, `trl_server_type`) VALUES ( 129, 129002, 52233, 0, '系统', 0, ' IT协助修改  692.66 这一条调整到广州 审批编号：ITSJ202410080005 ', 1728376945, 13);
+
+
+
+
+
+
+
+update tms_car_repair_monthlysettlement set tcrm_visible = 2  where tcrm_cr_id='11270';
+update car_repair set repair_visible = 2  where cr_id='11270';
+
+
+INSERT INTO `remark_log` (`rl_type`, `rl_subtype`, `rl_identifier`, `ur_uid`, `username`, `rl_status`, `rl_remark`, `rl_createtime`, `rl_server_type`, `rl_region_id`) VALUES ('170', '170008',11270, 0, '系统', 0, 'IT协助删除：  粤A90VF3车辆维修，提交流程重复，导致维保记录重复，需删除9月17号的记录   审批单号：ITSJ202410090006', 1728521031, 13, 0);
+
+
+
+update car_repair set trrb_id = 48  where trr_id='52110';
+
+INSERT INTO `tms_remark_log` (`trl_type`, `trl_subtype`, `trl_identifier`, `trl_uid`, `trl_username`, `trl_status`, `trl_remark`, `trl_createtime`, `trl_server_type`) VALUES ( 129, 129002, 52110, 0, '系统', 0, ' IT协助修改 999.33  这一条调整到上海 审批编号：ITSJ202410080005 ', 1728376945, 13);
+
+
+
+
+取派件补录优化
+feature_22403_csl_20241011  tms_admin
+https://project.ashsh.com.cn/index.php?m=task&f=view&taskID=22403   已上线
+
+
+
+冷藏车 新增客户温度预警推 推送
+feature_22409_csl_20241011   tms_admin
+https://project.ashsh.com.cn/index.php?m=task&f=view&taskID=22409  未上线
+
+
+
+
+取派件补录优化   已上线  10.12
+供应商小程序-修改代理发货限制   接口开发完成 10.8  
+冷藏车 新增客户温度预警推 推送   已上线 10.12
+供应商小程序-账号调整    进行中
+下周
+跟进 供应商小程序 接口对接
+
+
+
+
+支持删除君实订单附件
+feature_22407_csl_20241014   tms_admin
+https://project.ashsh.com.cn/index.php?m=task&f=view&taskID=22407  未上线
+
+202000453
+
+
+
+
+路桥费对账优化
+feature_22342_csl_20241015  tms_admin
+https://project.ashsh.com.cn/index.php?m=task&f=view&taskID=22342  未上线
+
+
+油耗报表导出优化
+feature_22460_csl_20241015
+https://project.ashsh.com.cn/index.php?m=task&f=view&taskID=22460  未上线
+
+
+供应商管理优化
+feature_22477_csl_20241016
+https://project.ashsh.com.cn/index.php?m=task&f=view&taskID=22477  已上线
+
+
+73201
+
+
+第二季度 服务之星 评优:
+
+
+1.路桥费账单共对账 48096 条，生成路桥费月结台账 22 条，对账总金额为 2104044.35 元 提高了工作效率。
+2.中石化加油记录共对账 8783 条，生成中石化月结台账 44 条，对账总金额为 6570510.36 元。
+3.中石油加油记录共对账 502 条，生成中石油月结台账 4 条，对账总金额为 196035.87 元。
+
+路桥费账单支持一键对账功能 提高了工作效率。
+
+
+
+
+1.路桥费账单共对账 48096 条，支持一键对账功能，显著提高了工作效率，生成路桥费月结台账 22 条，对账总金额为 2104044.35 元。
+
+
+中石化加油记录 支持一键对账功能， 自7月份以来共一键对账30个供应商 共对账 8783条加油记录 提高了对账效率
